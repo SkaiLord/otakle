39d56dc0c519e0e9b0ff673ea945376c77bcfca8
SkaiLord <skailord007@gmail.com>
2024-04-14 05:37:00 +0530

    add keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
+'use client';
+
 import { GuessRow } from '@/components/GuessRow';
+import { Keyboard } from '@/components/Keyboard';
 import { GAME_ROUNDS } from '@/lib/constants';
 import Image from 'next/image';
 
 export default function Home() {
+  const onKeyPress = (key: string) => {
+    console.log(key);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full max-w-xl flex flex-col items-center ">
+    <main className="pt-20 min-h-screen lg:pt-24 2xl:pt-28 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl flex flex-col items-center justify-between">
+      <div className="w-full max-w-xl flex flex-col items-center gap-8">
         <div className="flex flex-col gap-2">
           {Array.from({ length: GAME_ROUNDS }).map((_, idx) => (
             <GuessRow key={idx} letters="" />
           ))}
         </div>
+        <Keyboard onKeyPress={onKeyPress} />
       </div>
     </main>
   );
---
+++ components/Keyboard.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { FaBackspace } from 'react-icons/fa';
+import { Button } from './ui/button';
+
+const ROWS = [
+  'QWERTYUIOP'.split(''), //['Q','W'...]
+  'ASDFGHJKL'.split(''),
+  ['BACKSPACE', ...'ZXCVBNM'.split(''), 'ENTER'],
+];
+
+type Props = {
+  onKeyPress: (key: string) => void;
+};
+export const Keyboard = ({ onKeyPress }: Props) => {
+  return (
+    <div className="w-full flex flex-col gap-1.5">
+      {ROWS.map((letters, index) => {
+        return (
+          <div key={index} className="flex gap-1.5 w-full">
+            {letters.map((letter, idx) => {
+              return (
+                <Key
+                  key={letter as string}
+                  letter={letter}
+                  onKeyPress={onKeyPress}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+type KeyProps = {
+  letter: string;
+  onKeyPress: (key: string) => void;
+};
+
+export const Key = ({ letter, onKeyPress }: KeyProps) => {
+  return (
+    <Button
+      onClick={() => onKeyPress(letter)}
+      className={`${
+        letter != 'BACKSPACE' && letter != 'ENTER' ? ' flex-1' : 'flex-grow'
+      } p-1 text-xl bg-slate-600 hover:bg-slate-500`}
+      size={'lg'}
+      variant={'outline'}
+    >
+      {letter == 'BACKSPACE' ? <FaBackspace className="h-6 w-8" /> : letter}
+    </Button>
+  );
+};
