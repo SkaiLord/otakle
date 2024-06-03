6522296792645f24f7df49fce5a90b00694bb51c
SkaiLord <skailord007@gmail.com>
2024-06-04 01:46:27 +0530

    add game utilities

--- utils/isValidWord.ts
+++
@@ -1,7 +0,0 @@
-import allowedWords from '@/config/allowed_words.json';
-
-const allowedWordsSet = new Set(allowedWords);
-
-export const isValidWord = (word: string) => {
-  return allowedWordsSet.has(word.toLowerCase());
-};
